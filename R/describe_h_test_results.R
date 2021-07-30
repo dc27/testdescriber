1bfc7c7022018531358447fd103ef69a24bbf2d3
David Currie <dcurrie27@gmail.com>
2021-07-30 10:49:56 +0100

    adds more text to README. fixes documentation

--- R/describe_h_test_results.R
+++ R/describe_h_test_results.R
@@ -1,27 +1,27 @@
 #' Describe the results of a hypothesis test in 'plain' English.
 #'
-#' 
+#'
 #'
 #' @param p p value.
 #' @param alpha Confidence level. Default is 95%.
-#' @param h Named character vector containing text for the null and alternate hypothesis.
+#' @param h Named list vector containing text for the null and alternate hypothesis.
 #' @param stat Character vector containing type of observed statistic. Default is "mean"
 #' @param sf Number of significant figures to round p value in the output vector.
 #'
-#' @return A character vector 
+#' @return A character vector
 #' @export
 #'
 #' @examples
 #' standard usage:
 #' describe_h_test_results(
 #' 0.0123, 0.01,
-#' c(null = "Water is the best", alternative = "Water is not the best"),
+#' list(null = "Water is the best", alternative = "Water is not the best"),
 #' sf = 5, stat = "proportion"
 #' )
 #'
 
 describe_h_test_results <- function(
-  p, alpha = 0.05, h = NULL, stat = "mean", sf = 2
+  p, alpha = 0.05, h, stat = "mean", sf = 2
 ) {
   if (p <= alpha) {
     # reject null hypothesis, accept alternative hypothesis
@@ -29,21 +29,20 @@ describe_h_test_results <- function(
     add_alt_phrase <- paste0(
       " and accept the alternative hypothesis: ", h$alternative,
       ". It is unlikely that the difference between the observed ", stat,
-      " and the null hypothesis is due to chance, and instead represents a
-      statistically significant difference."
+      " and the null hypothesis is due to chance, and instead represents a statistically significant difference."
     )
   } else {
     allows <- "does not allow"
     add_alt_phrase <- paste0(
-      ". Based on the sample, there is not enough evidence that the ",  stat, 
+      ". Based on the sample, there is not enough evidence that the ",  stat,
       " is statistically significantly different from the null value.")
   }
-  
+
   phrase <- paste0(
     "Given a level of significance ", alpha, ", a p-value of ",round(p, sf),
     " ", allows, " the rejection of the null hypothesis: ",
     h$null, add_alt_phrase
   )
-  
+
   return(phrase)
 }
--- README.md
+++ README.md
@@ -8,3 +8,54 @@ Translate statistical test results into 'plain' English.
 devtools::install.github("dc27/testdescriber")
 library(testdescriber)
 ```
+
+# Preface
+
+It's difficult, sometimes, to put the results of a statistical test into words
+that all audiences can describe. Many times, I've gotten to the end of my
+experiment and forgotten exactly what the p-value I've calculated actually
+describes. This package hopes to alleviate the demands from having to describe
+a hypothesis test after forgetting statistical jargon.
+
+# Functions
+
+## `describe_h_test_results()`
+
+The main function (so far) of the package. It takes in your p-value, your
+alpha value (significance level), and a list containing your null hypothesis
+and alternative hypothesis:
+
+```
+h <- list()
+h$null <- "your null hypothesis"
+h$alternative <- "your alternative hypothesis"
+```
+
+Optional arguments include
+
+  - number of significant figures to round p value in the output string.
+  - observed statistic (e.g. stat = "mean")  
+
+
+```
+describe_h_test_results(
+  p = 0.0345,
+  alpha = 0.05,
+  h = list(
+    null = "Cats and dogs are the same",
+    alternative = "Cats and dogs are not the same"
+    ),
+  stat = "mean",
+  sf = 3
+  )
+```
+
+Output
+
+```
+[1] "Given a level of significance 0.05, a p-value of 0.034 allows the rejection
+of the null hypothesis: Cats and dogs are the same and accept the alternative
+hypothesis: Cats and dogs are not the same. It is unlikely that the difference
+between the observed mean and the null hypothesis is due to chance, and instead
+represents a statistically significant difference."
+```
